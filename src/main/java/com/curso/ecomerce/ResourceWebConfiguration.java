5f7101b31e61dae6d49b3c6b170a6c49cc66a524
RicardoLuengas <ricardo.luengas@outlook.com>
2024-06-16 12:55:08 -0600

    Funcionalidad ver productos para el usuario (metodo home, directorio, template/usuario.home.html)

--- src/main/java/com/curso/ecomerce/ResourceWebConfiguration.java
+++ src/main/java/com/curso/ecomerce/ResourceWebConfiguration.java
@@ -2,9 +2,10 @@ package com.curso.ecomerce;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ResourceWebConfiguration {
+public class ResourceWebConfiguration implements WebMvcConfigurer{
 	
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**").addResourceLocations("file:images/"); 
